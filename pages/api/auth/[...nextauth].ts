83e618927f19710f29bcef80fa75f9e534c2ba56
Kristian Ivanov <kriskopk@abv.bg>
2023-05-10 16:13:38 +0300

    Fixed error with datetime objects and integrated github login.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,40 +1,40 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth,{ AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import prisma from "@/app/libs/prismadb";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import bcrypt from "bcrypt";
-import  CredentialsProvider  from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions:AuthOptions = {
-    adapter:PrismaAdapter(prisma),
-    providers:[
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
         GithubProvider({
-            clientId:process.env.GITHUB_ID as string,
-            clientSecret:process.env.GITHUB_SECRET as string,
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
         }),
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
         CredentialsProvider({
-            name:'credentials',
-            credentials:{
-                email:{label:'email',type:'text'},
-                password:{label:'password',type:'password'},
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' },
             },
-            async authorize(credentials){
-                if(!credentials?.email || !credentials?.password){
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
                     throw new Error('Invalid credentials')
                 }
 
                 const user = await prisma.user.findUnique({
-                    where:{
-                        email:credentials.email
+                    where: {
+                        email: credentials.email
                     }
                 });
 
-                if(!user || !user?.hashedPassword){
+                if (!user || !user?.hashedPassword) {
                     throw new Error('Invalid credentials');
                 }
 
@@ -43,7 +43,7 @@ export const authOptions:AuthOptions = {
                     user.hashedPassword
                 );
 
-                if(!isCorrectPassword){
+                if (!isCorrectPassword) {
                     throw new Error('Invalid credentials');
                 }
 
@@ -51,14 +51,14 @@ export const authOptions:AuthOptions = {
             }
         })
     ],
-    pages:{
-        signIn:'/',
+    pages: {
+        signIn: '/',
     },
-    debug:process.env.NODE_ENV === 'development',
-    session:{
-        strategy:"jwt"
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt"
     },
-    secret:process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions);
