3e0fd35326ad54abd34be74bd4b65b5c917092d1
Kristian Ivanov <kriskopk@abv.bg>
2023-05-26 19:02:24 +0300

    Fixed mix of yarn/npm bug

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Select from 'react-select'
+
+const CountrySelect = () => {
+    
+
+  return (
+    <div className="">
+        
+    </div>
+  )
+}
+
+export default CountrySelect
