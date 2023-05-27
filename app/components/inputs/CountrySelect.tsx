0852949fe75df301eda814b58555b850fd342b22
Kristian Ivanov <kriskopk@abv.bg>
2023-05-27 16:39:19 +0300

    Added location,map component and more.

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,15 +1,54 @@
-'use client'
+"use client";
 
-import Select from 'react-select'
+import Select from "react-select";
+import useCountries from "../hooks/useCountries";
 
-const CountrySelect = () => {
-    
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+};
 
-  return (
-    <div className="">
-        
-    </div>
-  )
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
 }
 
-export default CountrySelect
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries();
+
+  return <div className="">
+    <Select 
+    placeholder="Anywhere"
+    isClearable
+    options={getAll()}
+    value={value}
+    onChange={(value)=>onChange(value as CountrySelectValue)}
+    formatOptionLabel={(option:any)=>(
+      <div className="flex flex-row items-center gap-3">
+        <div>{option.flag}</div>
+        <div>{option.label}, <span className="text-neutral-500 ml-1">{option.region}</span>
+        </div>
+      </div>
+    )}
+    classNames={{
+      control:()=>'p-3 border-2',
+      input:()=>'text-lg',
+      option:()=>'text-lg'
+    }}
+    theme={(theme)=>({
+      ...theme,
+      borderRadius:6,
+      colors:{
+        ...theme.colors,
+        primary:'black',
+        primary25:'#ffe4e6'
+      }
+    })}
+    />
+  </div>;
+};
+
+export default CountrySelect;
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -23,34 +24,45 @@ const RentModal = () => {
 
   const [step, setStep] = useState(STEPS.CATEGORY);
 
-  const{
-    register,handleSubmit,setValue,watch,formState:{
-      errors,
-    },reset
-  }= useForm<FieldValues>({
-    defaultValues:{
-      category:'',
-      location:null,
-      guestCount:1,
-      roomCount:1,
-      bathroomCount:1,
-      imageSrc:'',
-      price:1,
-      title:'',
-      descriptio:'',
-    }
-  })
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      descriptio: "",
+    },
+  });
 
-  const category = watch('category');
+  const category = watch("category");
+  const location = watch("location");
 
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
   //custom setValue because of rendering not working on setValue
-  const setCustomValue = (id:string,value:any)=>{
-    setValue(id,value,{
-      shouldDirty:true,
-      shouldTouch:true,
-      shouldValidate:true
-    })
-  }
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -67,50 +79,49 @@ const RentModal = () => {
     return "Next";
   }, [step]);
 
-  const secondaryActionLabel = useMemo(()=>{
-    if(step ===STEPS.CATEGORY){
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
       return undefined;
     }
 
-    return 'Back'
-  },[step])
-
+    return "Back";
+  }, [step]);
 
   let bodyContent = (
     <div className="flex flex-col gap-8">
-      <Heading 
-      title="Which of these best describes your place?"
-      subtitle="Pick category"
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick category"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-      {categories.map((item)=>(
-        <div key={item.label} className="col-span-1">
-          <CategoryInput 
-          onClick={(category)=>setCustomValue('category',category)}
-          selected={category === item.label}
-          label={item.label}
-          icon={item.icon}
-          />
-        </div>
-
-      ))}
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
       </div>
-
     </div>
-  )
+  );
 
-  if(step === STEPS.LOCATION){
-    bodyContent=(
-    <div className="flex flex-col gap-8">
-      <Heading 
-      title="Where is your place located?"
-      subtitle="Help guests find you!"
-      />
-      <CountrySelect 
-      
-      />
-    </div>
-    )
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
   }
 
   return (
