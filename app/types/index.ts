82bcf43db0aeaa63bd56c42a149e0d8babbfea22
Kristian Ivanov <kriskopk@abv.bg>
2023-06-16 15:36:58 +0300

    Individual Listing View.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,10 @@
-import { User } from "@prisma/client";
+import { User,Listing } from "@prisma/client";
+
+// we select from Listing createdAt
+export type SafeListing =Omit<Listing,"createdAt"> & {
+    createdAt:string;
+}
+
 
 export type SafeUser = Omit<User,
     "createdAt" | "updatedAt" | "emailVerified"
