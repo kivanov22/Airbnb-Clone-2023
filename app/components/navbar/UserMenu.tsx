1380061bfa405f7795926bb2df16335c02504f81
Kristian Ivanov <kriskopk@abv.bg>
2023-06-18 22:08:55 +0300

    My trips page.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -11,6 +11,7 @@ import useRentModal from "../../hooks/useRentModal";
 
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
+import { useRouter } from "next/navigation";
 
 interface UserMenuProps {
   currentUser?:SafeUser | null;
@@ -18,6 +19,7 @@ interface UserMenuProps {
 
 
 const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -62,7 +64,7 @@ const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
             <div className="flex flex-col cursor-pointer">
               {currentUser ? (
                  <>
-                 <MenuItem onClick={()=>{}} label="My trips"/>
+                 <MenuItem onClick={()=>router.push('/trips')} label="My trips"/>
                  <MenuItem onClick={()=>{}} label="My favorites"/>
                  <MenuItem onClick={()=>{}} label="My reservations"/>
                  <MenuItem onClick={rentModal.onOpen} label="Airbnb my home"/>
