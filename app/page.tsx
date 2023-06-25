eaf10fa118d6e068aba98787dcf99c9360762fc3
Kristian Ivanov <kriskopk@abv.bg>
2023-06-25 14:09:45 +0300

    My properties page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+const Home = async ({searchParams}:HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
-  if (listings.length ===0) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
-        <EmptyState showReset/>
+        <EmptyState showReset />
       </ClientOnly>
     );
   }
@@ -20,17 +24,19 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing)=>{
+          {listings.map((listing) => {
             return (
-             <ListingCard
-             currentUser={currentUser}
-             key={listing.id}
-             data={listing}
-             />
-            )
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
           })}
         </div>
       </Container>
     </ClientOnly>
   );
-}
+};
+
+export default Home;
