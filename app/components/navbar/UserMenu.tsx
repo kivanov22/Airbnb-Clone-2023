9541fb06911e7ebd69ba054492f9cd0621317893
Kristian Ivanov <kriskopk@abv.bg>
2023-06-25 13:33:01 +0300

    Favorite listings page.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -65,7 +65,7 @@ const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
               {currentUser ? (
                  <>
                  <MenuItem onClick={()=>router.push('/trips')} label="My trips"/>
-                 <MenuItem onClick={()=>{}} label="My favorites"/>
+                 <MenuItem onClick={()=>router.push('/favorites')} label="My favorites"/>
                  <MenuItem onClick={()=>router.push('/reservations')} label="My reservations"/>
                  <MenuItem onClick={rentModal.onOpen} label="Airbnb my home"/>
                  <hr />
